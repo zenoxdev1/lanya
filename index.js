6c81e780b705ea1515c28ae7c025515bc10b20f7
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2022-11-27 20:01:32 +0545

    init: adding functions

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const { token } = require("./settings");
 const client = new Lanya();
 module.exports = client;
 client.build(token);
-const app = module.exports = express();
+const app = (module.exports = express());
+app.set('views', path.join(__dirname, '/src'))
+app.set('view engine', 'ejs')
 
 
 app.use("/static", express.static(path.join(__dirname, "public")));
-app.use('/', require('./src/router/index'));
+app.use("/", require("./src/router/index"));
+app.use("/status", require("./src/router/status"));
+// Simple api endpoint
 
 if (!module.parent) {
   app.listen(3000);
-  console.log('Express started on port 3000');
+  console.log("Express started on port 3000");
 }
