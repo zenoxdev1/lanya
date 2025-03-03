1a5103e8626b63b4994ec4f0ce0fc25bb0c8ac06
birajrai <70630924+birajrai@users.noreply.github.com>
2025-03-03 15:04:13 +0545

    Fixes: Warning: useNewUrlParser is a deprecated option.

--- handlers/database.js
+++ handlers/database.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const { MongoDBURI } = require('./../config.json');
 module.exports = async () => {
   try {
-    await mongoose.connect(MongoDBURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MongoDBURI);
     console.log(global.styles.infoColor('✅ Connected to MongoDB'));
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error);
