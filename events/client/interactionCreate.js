169529e8f030a505af180612bb1a819335c7ff05
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2025-04-26 10:27:45 +0545

    Update interactionCreate.js

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -1,5 +1,4 @@
-const Giveaway = require('../../models/Giveaway');
-const { Events } = require('discord.js');
+const { Events, MessageFlags } = require('discord.js');
 
 module.exports = {
   name: Events.InteractionCreate,
@@ -21,54 +20,15 @@ module.exports = {
         if (interaction.replied || interaction.deferred) {
           await interaction.followUp({
             content: 'There was an error while executing this command!',
-            ephemeral: true,
+            flags: [MessageFlags.Ephemeral],
           });
         } else {
           await interaction.reply({
             content: 'There was an error while executing this command!',
-            ephemeral: true,
+            flags: [MessageFlags.Ephemeral],
           });
         }
       }
-    }
-
-    if (interaction.isButton() && interaction.customId === 'join_giveaway') {
-      const giveaway = await Giveaway.findOne({
-        messageId: interaction.message.id,
-        ongoing: true,
-      });
-
-      if (!giveaway) {
-        return interaction.reply({
-          content: 'This giveaway has ended or does not exist.',
-          ephemeral: true,
-        });
-      }
-
-      if (
-        giveaway.requiredRole &&
-        !interaction.member.roles.cache.has(giveaway.requiredRole)
-      ) {
-        return interaction.reply({
-          content: `You need the role <@&${giveaway.requiredRole}> to join this giveaway.`,
-          ephemeral: true,
-        });
-      }
-
-      if (giveaway.participants.includes(interaction.user.id)) {
-        return interaction.reply({
-          content: 'You are already participating in this giveaway!',
-          ephemeral: true,
-        });
-      }
-
-      giveaway.participants.push(interaction.user.id);
-      await giveaway.save();
-
-      await interaction.reply({
-        content: 'You have successfully joined the giveaway!',
-        ephemeral: true,
-      });
     }
 
     if (interaction.isAutocomplete()) {
